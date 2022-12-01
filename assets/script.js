5a82af26c858b79ffffdcd9336a152a905c27fd2
Nathalie Vanden Abeele <nath.vandenabeele@gmail.com>
2022-12-01 11:05:54 +0100

    Adding groups and structuring page and responsive.

--- assets/script.js
+++ assets/script.js
@@ -24,10 +24,138 @@ const KpopGroups = [
             "Yeosang",
             "Mingi",
             "Hongjoong",
-            "Yeosang",
+            "Yunho",
             "San",
             "Seonghwa",
             "Wooyoung"
+        ],
+    },
+    { 
+        name : "Monsta X",
+        agency : "Starship",
+        debutYear : 2015,
+        picture : "./assets/images/monstax.png",
+        type : "Groupe masculin",
+        members : [ 
+            "Hyungwon", 
+            "Kihyun",
+            "Jooheon",
+            "Shownu",
+            "IM",
+        ],
+    }, 
+     { 
+        name : "Twice",
+        agency : "JYP",
+        debutYear : 2015,
+        picture : "./assets/images/twice.jpg",
+        type : "Groupe féminin",
+        members : [ 
+            "Nayeon", 
+            "Momo",
+            "Sana",
+            "Tzuyu",
+            "Jeongyeon",
+            "Jihyo",
+            "Mina",
+            "Chaeyoung",
+            "Dahyun"
+        ],
+    },
+    { 
+        name : "Mamamoo",
+        agency : "RBW Entertainment",
+        debutYear : 2014,
+        picture : "./assets/images/mamamoo.jpg",
+        type : "Groupe féminin",
+        members : [ 
+            "Hwasa", 
+            "Wheein",
+            "Moonbyul",
+            "Solar",
+        ],
+    },
+    { 
+        name : "Seventeen",
+        agency : "HYBE",
+        debutYear : 2015,
+        picture : "./assets/images/svt.jpg",
+        type : "Groupe masculin",
+        members : [ 
+            "Mingyu", 
+            "Jeonghan",
+            "Wonwoo",
+            "Solar",
+            "Hoshi",
+            "Joshua",
+            "Jun",
+            "The8",
+            "DK",
+            "Woozi",
+            "S.Coups",
+            "Vernon",
+            "Seungkwan",
+            "Dino"
+        ],
+    }, 
+    { 
+        name : "Everglow",
+        agency : "YueHua Entertainment",
+        debutYear : 2019,
+        picture : "./assets/images/everglow.jpg",
+        type : "Groupe féminin",
+        members : [ 
+            "Yien", 
+            "Aisha",
+            "Sihyeon",
+            "Onda",
+            "Mia",
+            "E:U",
+        ],
+    },
+    { 
+        name : "Stray Kids",
+        agency : "JYP Entertainment",
+        debutYear : 2018,
+        picture : "./assets/images/straykids.jpg",
+        type : "Groupe masculin",
+        members : [ 
+            "Hyunjin", 
+            "Felix",
+            "Bang Chan",
+            "Lee Know",
+            "I;N",
+            "Han",
+            "Seungmin",
+            "Changbin",
+        ],
+    },
+    { 
+        name : "Oneus",
+        agency : "RBW Entertainment",
+        debutYear : 2019,
+        picture : "./assets/images/oneus.jpg",
+        type : "Groupe masculin",
+        members : [ 
+            "Xion", 
+            "Hwanwoong",
+            "Seoho",
+            "Leedo",
+            "Keonhee",
+            "Ravn"
+        ],
+    },
+    { 
+        name : "Ab6ix",
+        agency : "Brand New Music",
+        debutYear : 2019,
+        picture : "./assets/images/ab6ix.jpg",
+        type : "Groupe masculin",
+        members : [ 
+            "Jeon Woong", 
+            "Dongyun",
+            "Daehwi",
+            "Woojin",
         ],
     },
 ];
@@ -39,6 +167,8 @@ let container = document.getElementById("myCollection");
     
 }*/
 
+document.getElementById("numberGroup").innerHTML = KpopGroups.length;
+
 for (group in KpopGroups){
 
 let groupBox = document.createElement("section");
@@ -48,11 +178,19 @@ let groupImage = document.createElement("img");
 let groupContent = document.createElement("ul");
 let groupMembers = document.createElement("ul");
 
-let personTitle = document.createElement("b");
-let personTitleText = doculment.createTextNode("Members");
-
 groupBox.classList.add("groupItem");
 groupMembers.classList.add("members");
+
+if (KpopGroups[group]["type"] === "Groupe féminin"){
+    groupBox.classList.add("fem");
+
+} else if (KpopGroups[group]["type"] === "Groupe masculin") {
+    groupBox.classList.add("masc");
+} else {
+    groupBox.classList.add("mix");
+}
+
+groupContent.classList.add("groupContent");
 
 groupImage.src = KpopGroups[group]["picture"];
 
@@ -64,21 +202,18 @@ for (elem in KpopGroups[group]["members"]){
 }
 
 groupContent.innerHTML = `
-<li><b>Agence:</b>${KpopGroups[group]["agency"]}</li>
-<li><b>Année de début:</b>${KpopGroups[group]["debutYear"]}</li>
-<li><b>Type:</b>${KpopGroups[group]["type"]}</li>`;
+<li><b>Agence: </b>${KpopGroups[group]["agency"]}</li>
+<li><b>Année de début: </b>${KpopGroups[group]["debutYear"]}</li>
+<li><b>Type: </b>${KpopGroups[group]["type"]}</li>`;
 
 groupName.appendChild(groupNameText);
-
-personTitle.appendChild(personTitleText);
-
-groupMembers.appendChild(personTitle);
 
 groupBox.appendChild(groupName);
 groupBox.appendChild(groupImage);
 groupBox.appendChild(groupContent);
 groupBox.appendChild(groupMembers);
 container.appendChild(groupBox);
-
 }
+
+
 
